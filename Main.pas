c3aca3aa7f2567c1e7bbc03f36ab5d8d617693df
poxitron <poxitron1@gmail.com>
2015-07-02 20:23:12 +0200

    Formateo de código

--- Main.pas
+++ Main.pas
@@ -99,21 +99,21 @@ var
   QtyDroppedFiles, FileIndex: Integer;
   pDroppedFilename: array[0..255] of Char;
 begin
-  if Msg.message=WM_DROPFILES then
+  if Msg.message = WM_DROPFILES then
   begin
-    QtyDroppedFiles:=DragQueryFile(Msg.wParam, Cardinal(-1), nil, 0);
+    QtyDroppedFiles := DragQueryFile(Msg.wParam, Cardinal(-1), nil, 0);
     try
-      for FileIndex:=0 to QtyDroppedFiles -1 do
+      for FileIndex :=0 to QtyDroppedFiles -1 do
       begin
         DragQueryFile(Msg.wParam, FileIndex, @pDroppedFilename, sizeof(pDroppedFilename));
-        if Msg.hwnd=SourceFile_Edit.Handle then
+        if Msg.hwnd = SourceFile_Edit.Handle then
           begin
-           SourceFile_Edit.Text:=PChar(@pDroppedFilename);
+           SourceFile_Edit.Text := PChar(@pDroppedFilename);
           end;
       end;
     finally
       DragFinish(Msg.wParam);
-      Handled:=True;
+      Handled := True;
     end;
   end;
 end;
@@ -123,32 +123,34 @@ var
   i: Integer;
 begin
  for i := Low(FileNames) to High(FileNames) do
-  SourceFile_Edit.Text:=FileNames[i];
+ begin
+  SourceFile_Edit.Text := FileNames[i];
+ end;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin  //Asignar los valores a las variables
-  Merge:= ' -m';
-  Remove:= ' -r';
-  Verbose:= ' -v';
-  Framerate:=' -f 24000/1001';
+  Merge := ' -m';
+  Remove := ' -r';
+  Verbose := ' -v';
+  Framerate := ' -f 24000/1001';
   avsfile := ExtractFileDir(Application.ExeName) + '\temp.avs';
-  Application.OnMessage:=AppMessage;
+  Application.OnMessage := AppMessage;
   DragAcceptFiles(SourceFile_Edit.Handle, True);
   with Form1.Constraints do
     begin
-      MinWidth:=505;
-      MinHeight:=220;
-      MaxWidth:=0;
-      MaxHeight:=220;
+      MinWidth := 505;
+      MinHeight := 220;
+      MaxWidth := 0;
+      MaxHeight := 220;
     end;
   try  //Leer y cargar el archivo de configuración
     INI := TINIFile.Create(ExtractFileDir(Application.ExeName) + '\config.ini');
-    Merge_CheckBox.Checked:=INI.ReadBool('Config','Merge',true);
-    Remove_CheckBox.Checked:=INI.ReadBool('Config','Remove',true);
-    Verbose_CheckBox.Checked:=INI.ReadBool('Config','Verbose',true);
-    Framerate_ComboBox.ItemIndex:=INI.ReadInteger('Config','Framerate',0);
-    OpenDialog.InitialDir:=INI.ReadString('Config','Latest',ExtractFileDir(Application.ExeName));
+    Merge_CheckBox.Checked := INI.ReadBool('Config', 'Merge', True);
+    Remove_CheckBox.Checked := INI.ReadBool('Config', 'Remove', True);
+    Verbose_CheckBox.Checked := INI.ReadBool('Config', 'Verbose', True);
+    Framerate_ComboBox.ItemIndex := INI.ReadInteger('Config', 'Framerate', 0);
+    OpenDialog.InitialDir := INI.ReadString('Config', 'Latest', ExtractFileDir(Application.ExeName));
     Self.Top := INI.ReadInteger('form', 'MainFormTop', Screen.DesktopHeight div 2 - 120);
     Self.Left := INI.ReadInteger('form', 'MainFormLeft', Screen.DesktopWidth div 2 - 225);
     Self.Width := INI.ReadInteger('form', 'MainFormWidth', 500);
@@ -163,40 +165,40 @@ procedure TForm1.FormClose(Sender: TObject; var CloseAction: TCloseAction);
 begin
   DragAcceptFiles(SourceFile_Edit.Handle, False);
   try
-   INI:=TINIFile.Create(ExtractFileDir(Application.ExeName) + '\config.ini');
-   INI.WriteBool('Config','Merge',Merge_CheckBox.Checked);
-   INI.WriteBool('Config','Remove',Remove_CheckBox.Checked);
-   INI.WriteBool('Config','Verbose',Verbose_CheckBox.Checked);
-   INI.WriteInteger('Config','Framerate',Framerate_ComboBox.ItemIndex);
-   if SourceFile_Edit.Text<>'' then
-   begin
-     INI.WriteString('Config','Latest',ExtractFilePath(SourceFile_Edit.Text));
-   end;
-   INI.WriteInteger('form', 'MainFormTop', Self.Top);
-   INI.WriteInteger('form', 'MainFormLeft', Self.Left);
-   INI.WriteInteger('form', 'MainFormWidth', Self.Width);
-   INI.WriteInteger('form', 'MainFormHeight', Self.Height);
+    INI := TINIFile.Create(ExtractFileDir(Application.ExeName) + '\config.ini');
+    INI.WriteBool('Config', 'Merge', Merge_CheckBox.Checked);
+    INI.WriteBool('Config', 'Remove', Remove_CheckBox.Checked);
+    INI.WriteBool('Config', 'Verbose', Verbose_CheckBox.Checked);
+    INI.WriteInteger('Config', 'Framerate', Framerate_ComboBox.ItemIndex);
+    if SourceFile_Edit.Text <> '' then
+    begin
+      INI.WriteString('Config', 'Latest', ExtractFilePath(SourceFile_Edit.Text));
+    end;
+    INI.WriteInteger('form', 'MainFormTop', Self.Top);
+    INI.WriteInteger('form', 'MainFormLeft', Self.Left);
+    INI.WriteInteger('form', 'MainFormWidth', Self.Width);
+    INI.WriteInteger('form', 'MainFormHeight', Self.Height);
   finally
-   INI.Free;
+    INI.Free;
   end;
 end;
 
 procedure TForm1.ShowLog_ButtonClick(Sender: TObject);
 begin
- Form1.Constraints.MinHeight:=400;
- Form1.Constraints.MaxHeight:=0;
- Log_Memo.Visible:=True;
- ShowLog_Button.Visible:=False;
- HideLog_Button.Visible:=True;
+  Form1.Constraints.MinHeight := 400;
+  Form1.Constraints.MaxHeight := 0;
+  Log_Memo.Visible := True;
+  ShowLog_Button.Visible := False;
+  HideLog_Button.Visible := True;
 end;
 
 procedure TForm1.HideLog_ButtonClick(Sender: TObject);
 begin
-  Form1.Constraints.MinHeight:=220;
-  Form1.Constraints.MaxHeight:=220;
-  Log_Memo.Visible:=False;
-  HideLog_Button.Visible:=False;
-  ShowLog_Button.Visible:=True;
+  Form1.Constraints.MinHeight := 220;
+  Form1.Constraints.MaxHeight := 220;
+  Log_Memo.Visible := False;
+  HideLog_Button.Visible := False;
+  ShowLog_Button.Visible := True;
 end;
 
 
@@ -205,7 +207,7 @@ procedure TForm1.SourceFile_ButtonClick(Sender: TObject);
 begin
   if OpenDialog.Execute then
   begin
-    SourceFile_Edit.Text:=OpenDialog.Filename;
+    SourceFile_Edit.Text := OpenDialog.Filename;
   end;
 end;
 
@@ -214,15 +216,15 @@ procedure TForm1.SourceFile_EditChange(Sender: TObject);
 var
   f: string;
 begin
-  f:=ExtractFilePath(SourceFile_Edit.Text)+ExtractFileNameOnly(SourceFile_Edit.Text);
-  DestinationFile_Edit.Text:=f+'_split.mka';
+  f := ExtractFilePath(SourceFile_Edit.Text) + ExtractFileNameOnly(SourceFile_Edit.Text);
+  DestinationFile_Edit.Text := f + '_split.mka';
 end;
 
 procedure TForm1.DestinationFile_ButtonClick(Sender: TObject);
 begin
   if SaveDialog.Execute then
   begin
-    DestinationFile_Edit.Text:=SaveDialog.FileName;
+    DestinationFile_Edit.Text := SaveDialog.FileName;
   end;
 end;
 //-------------------------------
@@ -231,42 +233,62 @@ end;
 procedure TForm1.Merge_CheckBoxClick(Sender: TObject);
 begin
   if Merge_CheckBox.Checked then
-    Merge:=' -m'
+  begin
+    Merge := ' -m';
+  end
   else
-    Merge:='';
+  begin
+    Merge := '';
+  end;
 end;
 
 procedure TForm1.Remove_CheckBoxClick(Sender: TObject);
 begin
   if Remove_CheckBox.Checked then
-    Remove:=' -r'
+  begin
+    Remove := ' -r';
+  end
   else
-    Remove:='';
+  begin
+    Remove := '';
+  end;
 end;
 
 procedure TForm1.Verbose_CheckBoxClick(Sender: TObject);
 begin
   if Verbose_CheckBox.Checked then
-    Verbose:=' -v'
+  begin
+    Verbose := ' -v';
+  end
   else
-    Verbose:='';
+  begin
+    Verbose := '';
+  end;
 end;
 
 procedure TForm1.Framerate_ComboBoxSelect(Sender: TObject);
 begin
   if Framerate_ComboBox.ItemIndex = 0 then
-    Framerate:=' -f 24000/1001'
+  begin
+    Framerate := ' -f 24000/1001';
+  end
   else
-    if Framerate_ComboBox.ItemIndex = 1 then
-      Framerate:=' -f 30000/1001';
+  if Framerate_ComboBox.ItemIndex = 1 then
+  begin
+    Framerate := ' -f 30000/1001';
+  end;
 end;
 
 procedure TForm1.AddTrim_ButtonClick(Sender: TObject);
 begin
-  if Trims_Edit.Text='' then
-    Trims_Edit.Text:='trim(0,0)'
+  if Trims_Edit.Text = '' then
+  begin
+    Trims_Edit.Text := 'trim(0,0)';
+  end
   else
-    Trims_Edit.Text:=Trims_Edit.Text+'+trim(0,0)';
+  begin
+    Trims_Edit.Text := Trims_Edit.Text + '+trim(0,0)';
+  end;
 end;
 
 procedure TForm1.DeleteTrim_ButtonClick(Sender: TObject);
@@ -274,10 +296,12 @@ var
   RegEx: TRegEx;
   M: TMatch;
 begin
-  RegEx.Create('\+trim\([0-9]+,[0-9]+\)$',[roSingleLine]);
+  RegEx.Create('\+trim\([0-9]+,[0-9]+\)$', [roSingleLine]);
   M := Regex.Match(Trims_Edit.Text);
   if M.Success then
-    Trims_Edit.Text:= RegEx.Replace(Trims_Edit.Text,'');
+  begin
+    Trims_Edit.Text := RegEx.Replace(Trims_Edit.Text, '');
+  end;
 end;
 
 //Inicar el proceso de cortado del audio
@@ -285,26 +309,30 @@ procedure TForm1.CutButtonClick(Sender: TObject);
 var
   AStringList: TStringList;
 begin
-  input:=SourceFile_Edit.Text;
-  output:=DestinationFile_Edit.Text;
-  ejecutable:='"'+ExtractFileDir(Application.ExeName)+'\split_aud.exe "';
-  parametros:=Merge+Remove+Verbose+Framerate+' -i "'+input+'" -o "'+output+'" "'+avsfile+'"';
-  if input='' then
-    MessageDlg('No has seleccionado ningún archivo para cortar.', mtError, [mbOK], 0)
+  input := SourceFile_Edit.Text;
+  output := DestinationFile_Edit.Text;
+  ejecutable := '"' + ExtractFileDir(Application.ExeName) + '\split_aud.exe "';
+  parametros := Merge + Remove + Verbose + Framerate + ' -i "' + input + '" -o "' + output + '" "' + avsfile + '"';
+  if input = '' then
+  begin
+    MessageDlg('No has seleccionado ningún archivo para cortar.', mtError, [mbOK], 0);
+  end
   else
   if not FileExists(input) then
-    MessageDlg('El archivo de origen no existe.', mtError, [mbOK], 0)
+  begin
+    MessageDlg('El archivo de origen no existe.', mtError, [mbOK], 0);
+  end
   else
   try
-    Estado_Label.Caption:='Estado: Cortando el archivo...';
-    AStringList:=TStringList.Create;
+    Estado_Label.Caption := 'Estado: Cortando el archivo...';
+    AStringList := TStringList.Create;
     AStringList.Add(Trims_Edit.Text);
     AStringList.SaveToFile(avsfile);
     Log_Memo.Clear;
     CaptureConsoleOutputMod(ejecutable, parametros, Log_Memo);
+    Estado_Label.Caption := 'Estado: Operación finalizada.';
   finally
     AStringList.Free;
-    Estado_Label.Caption:='Estado: Operación finalizada.';
   end;
 end;
 
